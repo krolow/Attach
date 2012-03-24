1b5707b5477383104783c203459579c7710335ba
Moacir Rosa <mrosa@conrad-caine.com>
2012-03-24 18:54:39 -0300

    Applying CakePHPPear standard: schema.php

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -1,37 +1,108 @@
 <?php
 /**
-* Attachment schema
-*
-*
-* PHP 5.3
-*
-*
-* Licensed under The MIT License
-* Redistributions of files must retain the above copyright notice.
-*
-* @link          https://github.com/krolow/Attach
-* @package       Attach.Model.UploadBehavior
-* @author		 Vinícius Krolow <krolow@gmail.com>
-* @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
-*/
-Ap
+ * AttachmentSchema
+ * 
+ * PHP Version 5.3+
+ *
+ * @category Plugin
+ * @package  Attachment.Schema
+ * @author   Vinícius Krolow <krolow@gmail.com>
+ * @license  GNU GENERAL PUBLIC LICENSE
+ * @link     https://github.com/krolow/Attach
+ */
+
+/**
+ * AttachmentSchema
+ * 
+ * @category Plugin
+ * @package  Attachment.Schema
+ * @author   Vinícius Krolow <krolow@gmail.com>
+ * @license  GNU GENERAL PUBLIC LICENSE
+ * @link     https://github.com/krolow/Attach
+ */
 class AttachmentSchema extends CakeSchema
 {
+    /**
+     * Before callback
+     *
+     * @param Array $event Event
+     * 
+     * @return Boolean
+     */
+    public function before($event = array())
+    {
+        return true;
+    }
 
-	public function before($event = array()) {
-		return true;
-	}
+    /**
+     * After callback
+     *
+     * @param Array $event Event
+     * 
+     * @return Boolean
+     */
+    public function after($event = array())
+    {
+    
+    }
 
-	public function after($event = array()) {
-	}
+    public $attachments = array(
 
-	public $attachments = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary', 'collate' => NULL, 'comment' => ''),
-		'filename' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'comment' => '', 'charset' => 'utf8'),
-		'model' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 150, 'collate' => 'utf8_general_ci', 'comment' => '', 'charset' => 'utf8'),
-		'foreign_key' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'collate' => NULL, 'comment' => ''),
-		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'comment' => '', 'charset' => 'utf8'),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
+        'id' => array(
+            'type' => 'integer',
+            'null' => false,
+            'default' => null,
+            'key' => 'primary',
+            'collate' => null,
+            'comment' => ''
+        ),
+
+        'filename' => array(
+            'type' => 'string',
+            'null' => false,
+            'default' => null,
+            'length' => 150,
+            'collate' => 'utf8_general_ci',
+            'comment' => '',
+            'charset' => 'utf8'
+        ),
+
+        'model' => array(
+            'type' => 'string',
+            'null' => false,
+            'default' => null,
+            'length' => 150,
+            'collate' => 'utf8_general_ci',
+            'comment' => '',
+            'charset' => 'utf8'
+        ),
+
+        'foreign_key' => array(
+            'type' => 'integer',
+            'null' => false,
+            'default' => null,
+            'collate' => null,
+            'comment' => ''
+        ),
+
+        'type' => array(
+            'type' => 'string',
+            'null' => false,
+            'default' => null,
+            'length' => 100,
+            'collate' => 'utf8_general_ci',
+            'comment' => '',
+            'charset' => 'utf8'
+        ),
+
+        'indexes' => array(
+            'PRIMARY' => array('column' => 'id', 'unique' => 1)
+        ),
+
+        'tableParameters' => array(
+            'charset' => 'utf8',
+            'collate' => 'utf8_general_ci',
+            'engine' => 'InnoDB'
+        )
+    );
 }
