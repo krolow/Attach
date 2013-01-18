1e789cbd986bd985f425575841fe543180504a37
Vinícius Krolow <krolow@gmail.com>
2013-01-18 17:21:32 -0200

    Configuring to start the tests

--- Test/Case/UploadBehaviorTest.php
+++ Test/Case/UploadBehaviorTest.php
@@ -3,17 +3,90 @@ App::uses('Attach.Upload', 'Model/Behavior');
 
 class UploadBehaviorTest extends CakeTestCase {
 
-    public $fixtures = array('plugin.attach.attachment');
+    public $fixtures = array(
+        'plugin.attach.attachment',
+        'core.article'
+    );
 
     public function setUp() {
-        parent::setUp();        
+        parent::setUp();
+        mkdir(APP . 'tmp' . DS . 'Attach', 0777, true);
+        $this->Article = ClassRegistry::init('Article');
+    }
+
+    protected function getConfig() {
+        return array(
+            'home' => array(
+                'dir' => 'tmp{DS}Attach{DS}',
+                'thumbs' => array(
+                    'thumb' => array(
+                        'w' => 100,
+                        'h' => 100,
+                        'crop' => true
+                    )
+                )
+            )
+        );  
     }
 
     public function tearDown() {
+        rmdir(APP . 'tmp' . DS . 'Attach');
         parent::tearDown();
     }
 
     public function testUpload() {
+
+    }
+
+    public function testIfPostFileDataIsEmpty() {
+
+    }
+
+    public function testIfFileIsRequired() {
+
+    }
+
+    public function testValidateExtension() {
+
+    }
+
+    public function testValidateMime() {
+
+    }
+
+    public function testValidateSize() {
+
+    }
+
+    public function testValidateMaxDimensions() {
+
+    }
+
+    public function testValidateMinDimensions() {
+
+    }
+
+    public function testFileMime() {
+
+    }
+
+    public function testFileExtension() {
+
+    }
+
+    public function testUploadFolder() {
+
+    }
+
+    public function testIfDirectoryIsWritable() {
+
+    }
+
+    public function testGenerateName() {
+
+    }
+
+    public function testThumbCreation() {
 
     }
 
