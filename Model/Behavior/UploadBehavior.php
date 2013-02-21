3f6a87bc3d8ddb27c8820c756bffeaecc90bbe17
Benjamin Young <byoung@bigbluehat.com>
2013-02-21 14:39:03 -0500

    fixed extension()
    $model was missing in getFileExtension call

--- Model/Behavior/UploadBehavior.php
+++ Model/Behavior/UploadBehavior.php
@@ -188,6 +188,7 @@ class UploadBehavior extends ModelBehavior
         if (isset($check['name'])) {
             return in_array(
                 $this->getFileExtension(
+                    $model,
                     $check['name']
                 ),
                 $extensions
